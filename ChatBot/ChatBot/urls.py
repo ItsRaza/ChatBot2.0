6f12d7b1ce2c65711d857cd899ea2e91c24883d2
Syed Raza Abbas <you@example.com>
2019-05-07 13:05:57 -0700

    urls updated a/c to my need

--- ChatBot/ChatBot/urls.py
+++ ChatBot/ChatBot/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from .views import chat
 
 urlpatterns = [
-    url(r'^chat/$', chat, name='chat'),
+    url(r'chat/$', chat, name='chat'),
     url(r'^admin/', admin.site.urls)
 ]
